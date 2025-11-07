b3f6577787ff63b96a5236ccb1c2a139b5c7b938
estellepaz12 <117852183+estellepaz12@users.noreply.github.com>
2025-11-07 15:22:29 -0500

    FIX: Included essential libraries (unordered_map, memory, cmath)

--- trie.cpp
+++ trie.cpp
@@ -2,12 +2,15 @@
 #define TRIE_H
 
 #include <iostream>
-#include<algorithm>
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <sstream>
 #include <tuple>
 #include <cctype>
+#include <cmath>
+#include <unordered_map>
+#include <memory>
 #include "trie.h"
 #include <algorithm>
 using namespace std;
