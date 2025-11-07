6533994f41f1580f2b3f49506e4e65452aab300d
estellepaz12 <117852183+estellepaz12@users.noreply.github.com>
2025-11-07 15:17:26 -0500

    FEAT: Trie accommodates all chars, frequency returns proportions

--- trie.cpp
+++ trie.cpp
@@ -12,28 +12,24 @@
 #include <algorithm>
 using namespace std;
 
-monolith_trie::monolith_trie() {
-	root = new trie_node();
+monolith_trie::monolith_trie() : root(make_unique<trie_node>()) {
+	count = 0;
 }
 
 void monolith_trie::insert(string word) {
-	trie_node* curr = root;
+	trie_node* curr = root.get();
 	for (char c : word) {
-		if (isalpha(c)) {
-			c = tolower(c);
-			int index = c - 'a';
-			if (!curr->children[index]) {
-				curr->children[index] = new trie_node();
-			}
-			curr = curr->children[index];
+		if (curr->children.find(c) == curr->children.end()) {
+			curr->children[c] = make_unique<trie_node>();
 		}
+		curr = curr->children[c].get();
 	}
 	curr->end_word = true;
 	curr->frequency++;
+	count++;
 }
 
 void monolith_trie::insert_sentence(string sentence) {
-	trie_node* curr = root;
 	if (sentence.find(' ') == string::npos) {
 		insert(sentence);
 	} else {
@@ -46,13 +42,12 @@ void monolith_trie::insert_sentence(string sentence) {
 }
 
 void monolith_trie::remove(string word) {
-	trie_node* curr = root;
+	trie_node* curr = root.get();
 	for (char c : word) {
-		int index = c - 'a';
-		if (!curr->children[index]) {
+		if (curr->children.find(c) == curr->children.end()) {
 			return;
 		}
-		curr = curr->children[index];
+		curr = curr->children[c].get();
 	}
 	if (curr->end_word == true) {
 		curr->end_word = false;
@@ -60,60 +55,41 @@ void monolith_trie::remove(string word) {
 }
 
 bool monolith_trie::search(string word) {
-	trie_node* curr = root;
+	trie_node* curr = root.get();
 	for (char c : word) {
-		int index = c - 'a';
-		if (!curr->children[index]) {
+		if (curr->children.find(c) == curr->children.end()) {
 			return false;
 		}
-		curr = curr->children[index];
+		curr = curr->children[c].get();
 	}
-	return curr->end_word;
+	return curr != nullptr && curr->end_word;
 }
 
 bool monolith_trie::starts_with(string prefix) {
-	trie_node* curr = root;
+	trie_node* curr = root.get();
 	for (char c : prefix) {
-		int index = c - 'a';
-		if (!curr->children[index]) {
+		if (curr->children.find(c) == curr->children.end()) {
 			return false;
 		}
-		curr = curr->children[index];
+		curr = curr->children[c].get();
 	}
 	return true;
 }
 
-
-void monolith_trie::print(trie_node* node, string prefix) const {
-	if (node->end_word) {
-		cout << prefix << endl;
-	}
-	for (int i = 0; i < 26; i++) {
-		if (node->children[i]) {
-			print(node->children[i], prefix + char(i + 'a'));
-		}
-	}
-}
-
-void monolith_trie::print() const {
-	print(root, "");
-}
-
 void monolith_trie::collect_words(trie_node* node, string current, vector<pair<string, float>>& words) {
 	if (node->end_word) {
-		words.push_back({current, node->frequency});
+		float freq_prop = (float)node->frequency / (float)count;
+		words.push_back({current, freq_prop});
 	}
-	for (int i = 0; i < 26; i++) {
-		if (node->children[i]) {
-			char c = i + 'a';
-			collect_words(node->children[i], current + c, words);
-		}
+	for (auto const& [key, val] : node->children) {
+		trie_node* curr = val.get();
+		collect_words(curr, current + key, words);
 	}
 }
 
 tuple<vector<string>, vector<float>> monolith_trie::top_k(int k) {
 	vector<pair<string, float>> all_words;
-	collect_words(root, "", all_words);
+	collect_words(root.get(), "", all_words);
 
 	sort(all_words.begin(), all_words.end(), [](const auto& a, const auto& b) {
 		return a.second > b.second;
@@ -122,7 +98,15 @@ tuple<vector<string>, vector<float>> monolith_trie::top_k(int k) {
 	vector<string> words;
 	vector<float> freqs;
 	for (int i = 0; i < min((int)all_words.size(), k); i++) {
-		words.push_back(all_words[i].first);
+		string lower_word = "";
+		for (char c : all_words[i].first) {
+			if (isalpha(c)) {
+				lower_word += tolower(c);
+			} else {
+				lower_word += c;
+			}
+		}
+		words.push_back(lower_word);
 		freqs.push_back(all_words[i].second);
 	}
 
@@ -131,7 +115,7 @@ tuple<vector<string>, vector<float>> monolith_trie::top_k(int k) {
 
 tuple<vector<string>, vector<float>> monolith_trie::bottom_k(int k) {
 	vector<pair<string, float>> all_words;
-	collect_words(root, "", all_words);
+	collect_words(root.get(), "", all_words);
 
 	sort(all_words.begin(), all_words.end(), [](const auto& a, const auto& b) {
 		return a.second < b.second;
@@ -140,7 +124,15 @@ tuple<vector<string>, vector<float>> monolith_trie::bottom_k(int k) {
 	vector<string> words;
 	vector<float> freqs;
 	for (int i = 0; i < min((int)all_words.size(), k); i++) {
-		words.push_back(all_words[i].first);
+		string lower_word = "";
+		for (char c : all_words[i].first) {
+			if (isalpha(c)) {
+				lower_word += tolower(c);
+			} else {
+				lower_word += c;
+			}
+		}
+		words.push_back(lower_word);
 		freqs.push_back(all_words[i].second);
 	}
 
@@ -148,16 +140,16 @@ tuple<vector<string>, vector<float>> monolith_trie::bottom_k(int k) {
 }
 
 float monolith_trie::identify(string word) {
-	trie_node* curr = root;
+	trie_node* curr = root.get();
 	for (char c : word) {
-		int index = c - 'a';
-		if (!curr->children[index]) {
+		if (curr->children.find(c) == curr->children.end()) {
 			return 0;
 		}
-		curr = curr->children[index];
+		curr = curr->children[c].get();
 	}
 	if (curr != nullptr && curr->end_word) {
-		return curr->frequency;
+		float freq_prop = (float)curr->frequency / (float)count;
+		return freq_prop;
 	}
 	return 0;
 }
